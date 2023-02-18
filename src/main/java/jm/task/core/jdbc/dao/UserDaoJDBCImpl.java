54333dec749153635e8e9f598d708165a48db3c3
RomanStolov <roman_stolov@mail.ru>
2023-02-18 06:35:27 +0300

    1. Вынес в отдельные переменные sql-команды. 2. В классе Util вынес в отдельные переменные параметры подключения. 3. Добавил подключение Driver для обратной совместимости в классе Util.

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -14,26 +14,28 @@ public class UserDaoJDBCImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS users (" +
+                "id BIGINT AUTO_INCREMENT, " +
+                "name VARCHAR(25) NOT NULL, " +
+                "lastName VARCHAR(25) NOT NULL, " +
+                "age TINYINT NOT NULL, " +
+                "CHECK((age > 0 AND age < 100) AND (name != '') AND (lastName != '')), " +
+                "PRIMARY KEY (id))";
         try (Connection connection = Util.getConnection();
              Statement statement = connection.createStatement()) {
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS users (" +
-                    "id BIGINT AUTO_INCREMENT, " +
-                    "name VARCHAR(25) NOT NULL, " +
-                    "lastName VARCHAR(25) NOT NULL, " +
-                    "age TINYINT NOT NULL, " +
-                    "CHECK((age > 0 AND age < 100) AND (name != '') AND (lastName != '')), " +
-                    "PRIMARY KEY (id))");
-        } catch (SQLException e) {
+            statement.executeUpdate(sql);
+        } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
 
     @Override
     public void dropUsersTable() {
+        String sql = "DROP TABLE IF EXISTS users";
         try (Connection connection = Util.getConnection();
              Statement statement = connection.createStatement()) {
-            statement.executeUpdate("DROP TABLE IF EXISTS users");
-        } catch (SQLException e) {
+            statement.executeUpdate(sql);
+        } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
@@ -41,14 +43,15 @@ public class UserDaoJDBCImpl implements UserDao {
 //    // Вариант №1
 //    @Override
 //    public void saveUser(String name, String lastName, byte age) {
+//        String sql = "INSERT INTO users (name, lastName, age) VALUES ('" +
+////                    name + "', '" +
+////                    lastName + "', " +
+////                    age + ")";
 //        try (Connection connection = Util.getConnection();
 //             Statement statement = connection.createStatement()) {
-//            statement.executeUpdate("INSERT INTO users (name, lastName, age) VALUES ('" +
-//                    name + "', '" +
-//                    lastName + "', " +
-//                    age + ")");
+//            statement.executeUpdate(sql);
 //            System.out.println("User с именем – " + name + " добавлен в базу данных");
-//        } catch (SQLException e) {
+//        } catch (SQLException | ClassNotFoundException e) {
 //            throw new RuntimeException(e);
 //        }
 //    }
@@ -56,15 +59,15 @@ public class UserDaoJDBCImpl implements UserDao {
     // Вариант 2
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        String sql = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)";
         try (Connection connection = Util.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(
-                     "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)")) {
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setByte(3, age);
             preparedStatement.executeUpdate();
             System.out.println("User с именем – " + name + " добавлен в базу данных");
-        } catch (SQLException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
@@ -72,10 +75,11 @@ public class UserDaoJDBCImpl implements UserDao {
 //    // Вариант №1
 //    @Override
 //    public void removeUserById(long id) {
+//        String sql = "DELETE FROM users WHERE id = " + (long) id;
 //        try (Connection connection = Util.getConnection();
 //             Statement statement = connection.createStatement()) {
-//            statement.executeUpdate("DELETE FROM users WHERE id = " + (long) id);
-//        } catch (SQLException e) {
+//            statement.executeUpdate(sql);
+//        } catch (SQLException | ClassNotFoundException e) {
 //            throw new RuntimeException(e);
 //        }
 //    }
@@ -83,11 +87,12 @@ public class UserDaoJDBCImpl implements UserDao {
     // Вариант №2
     @Override
     public void removeUserById(long id) {
+        String sql = "DELETE FROM users WHERE id = ?";
         try (Connection connection = Util.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM users WHERE id = ?")) {
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             preparedStatement.setLong(1, id);
             preparedStatement.executeUpdate();
-        } catch (SQLException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
@@ -95,9 +100,10 @@ public class UserDaoJDBCImpl implements UserDao {
     @Override
     public List<User> getAllUsers() {
         List<User> userList = new ArrayList<>();
+        String sql = "SELECT * FROM users";
         try (Connection connection = Util.getConnection();
              Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
+            ResultSet resultSet = statement.executeQuery(sql);
             while (resultSet.next()) {
                 User userTemp = new User();
                 userTemp.setId(resultSet.getLong("id"));
@@ -106,7 +112,7 @@ public class UserDaoJDBCImpl implements UserDao {
                 userTemp.setAge(resultSet.getByte("age"));
                 userList.add(userTemp);
             }
-        } catch (SQLException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
         return userList;
@@ -114,10 +120,11 @@ public class UserDaoJDBCImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
+        String sql = "TRUNCATE TABLE users";
         try (Connection connection = Util.getConnection();
              Statement statement = connection.createStatement()) {
-            statement.executeUpdate("TRUNCATE TABLE users");
-        } catch (SQLException e) {
+            statement.executeUpdate(sql);
+        } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,8 +5,13 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/mydb";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "root";
 
-    public static Connection getConnection() throws SQLException {
-            return DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "root");
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName(DB_DRIVER);
+        return DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
         }
 }
